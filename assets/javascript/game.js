b571e7d43dfa2d16783920b1032b4b63f64a7331
Melika Kalbasi <melika_kalbasi@melikas-air.gateway.sonic.net>
2018-11-11 16:30:04 -0800

    psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+$(document).ready(function() { 
+
+    // declare global variables
+    var crystalOne = 0;
+    var crystalTwo = 0;
+    var crystalThree = 0;
+    var crystalFour = 0;
+
+    // use a function to initialize game, and have it ready for reset
+
+    // randomly generate number displayed at start of game
+
+    // randomly generate number for each crystal (hidden)
+
+    // each click will add a number to result panel
+
+    // when number matches, results in 1 win
+
+    // if loss, losses increase by 1
+
+    // games resets, wins and losses are still tracked
+
+   
+    
+
+
+
+
+
+
+
+
+});
